41eb6997ad2c17ea1b09828da45f555b08e0f2e4
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-06-05 09:58:28 +0800

    Add new lesson

--- app/src/main/java/com/group5/quizApp/Config.java
+++ app/src/main/java/com/group5/quizApp/Config.java
@@ -2,6 +2,7 @@ package com.group5.quizApp;
 
 public class Config
 {
-	public static final int DB_VERSION = 5;
+	public static final int DB_VERSION = 6;
 	public static final String YOUTUBE_API_KEY = "AIzaSyDhvRljHPs7I1bYiNc5US5l1ee1oorGFSU";
 }
+
